881ec5d8677a2d0d97fc7366b34b3d28f84e9b90
Uexcel2022 <excellenceudoka@gmail.com>
2024-10-22 21:31:33 +0100

    added configurations and improvements

--- src/main/java/com/eazybyties/card/dto/CardContactInfoDto.java
+++ src/main/java/com/eazybyties/card/dto/CardContactInfoDto.java
@@ -3,6 +3,8 @@ package com.eazybyties.card.dto;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -13,6 +15,9 @@ import java.util.Map;
         description = "Schema to hold contact information"
 )
 @ConfigurationProperties(prefix = "card")
-public record CardContactInfoDto(String message, Map<String,
-        String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class CardContactInfoDto{
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
